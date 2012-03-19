473cb302daba69d5fb4db521aa60d0fd2dfa1fa9
Jaime Hoffman <hwbtthmyanks@gmail.com>
2012-03-18 21:23:12 -0400

    User/Micropost associations done

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,8 @@ class User < ActiveRecord::Base
 	attr_accessor :password
 	attr_accessible(:name, :email, :password, :password_confirmation)	#make these attributes accessible
 
+  has_many :microposts, :dependent => :destroy
+
 	email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   ### USER ATTRIBUTE VALIDATIONS #########################################
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,11 +1,9 @@
 # By using the symbol ':user', we get Factory Girl to simulate the User model.
 Factory.define :user do |user|
-
 	user.name										"Example User"
 	user.email									"user@example.com"
 	user.password 							"foobar"
 	user.password_confirmation	"foobar"
-
 end
 
 Factory.sequence :name do |n|
@@ -14,4 +12,9 @@ end
 
 Factory.sequence :email do |n|
 	"Person-#{n}@example.com"
+end
+
+Factory.define :micropost do |micropost|
+	micropost.content "foo bar"
+	micropost.association :user
 end
